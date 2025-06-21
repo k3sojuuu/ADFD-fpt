c77920e3b999f6195c08aeb18ed0ab2e73df94f7
tannt <129242298+k3sojuuu@users.noreply.github.com>
2025-06-21 11:20:02 +0700

    backend

---
+++ demo/src/main/java/com/example/backendadfd/service/placeService/PlaceService.java
@@ -0,0 +1,9 @@
+package com.example.backendadfd.service.placeService;
+
+import com.example.backendadfd.model.Place;
+
+import java.util.List;
+
+public interface PlaceService {
+    List<Place> getAllPlaces();
+}
